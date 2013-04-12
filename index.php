7acd8b35dd37d462b273db98ef9232a750e06bcf
David Konsumer <konsumer@jetboystudio.com>
2013-04-12 12:50:59 -0700

    starting to add support for git

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ if (empty($_POST['payload'])){
 	die('bad.');
 }
 
+require_once('Git.php');
+
+$authorized = array('konsumer');
+
 $n = getdate();
 
 $timestamp = $n['mon'] . '-' . $n['mday'] . '-' . $n['year'] . ' '. $n['hours'] . ':' . $n['minutes'] . ':' . $n['seconds'];
@@ -12,3 +16,11 @@ $timestamp = $n['mon'] . '-' . $n['mday'] . '-' . $n['year'] . ' '. $n['hours'] 
 file_put_contents('log.txt', $_SERVER['REMOTE_ADDR'] . ' - ' . $timestamp . ' - ' . $_POST['payload'] . "\n", FILE_APPEND | LOCK_EX);
 
 $payload = json_decode($_POST['payload'], true);
+
+foreach($payload['commmits'] as $commit){
+	if (in_array($commit['committer']['username'], $authorized)){
+		$repo_dir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'githook';
+
+		$repo = Git::open($repo_dir, str_replace('https://', 'git@', $payload['repository']['url']) . '.git'));
+	}
+}
