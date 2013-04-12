42fd55846b68a581f1ad025b055c7d8adab98194
David Konsumer <konsumer@jetboystudio.com>
2013-04-11 22:31:48 -0700

    more tests

--- index.php
+++ index.php
@@ -4,8 +4,11 @@ if (empty($_POST['payload'])){
 	die('bad.');
 }
 
+$n = getdate();
+
+$timestamp = $n['mon'] . '-' . $n['mday'] . '-' . $n['year'] . ' '. $n['hours'] . ':' . $n['minutes'] . ':' . $n['seconds'];
+
 // save request to log
-file_put_contents('log.txt', $_POST['payload']+"\n\n", FILE_APPEND | LOCK_EX);
+file_put_contents('log.txt', $_SERVER['REMOTE_ADDR'] . ' - ' . $timestamp . ' - ' . $_POST['payload'] . "\n", FILE_APPEND | LOCK_EX);
 
 $payload = json_decode($_POST['payload'], true);
-
