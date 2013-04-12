0711e17b91e9e7032c4986e1cd5100f15d11e9ce
David Konsumer <konsumer@jetboystudio.com>
2013-04-12 13:00:50 -0700

    better temp dir

--- index.php
+++ index.php
@@ -17,10 +17,16 @@ file_put_contents('log.txt', $_SERVER['REMOTE_ADDR'] . ' - ' . $timestamp . ' - 
 
 $payload = json_decode($_POST['payload'], true);
 
+$do_pull = FALSE;
+
 foreach($payload['commmits'] as $commit){
 	if (in_array($commit['committer']['username'], $authorized)){
-		$repo_dir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'githook';
-
-		$repo = Git::open($repo_dir, str_replace('https://', 'git@', $payload['repository']['url']) . '.git'));
+		$do_pull = TRUE;
+		break;
 	}
+}
+
+if ($do_pull){
+	$repo_dir = tempnam(sys_get_temp_dir(), 'github-hook'); unlink($repo_dir); mkdir($repo_dir);
+	$repo = Git::open($repo_dir, str_replace('https://', 'git@', $payload['repository']['url']) . '.git'));
 }
